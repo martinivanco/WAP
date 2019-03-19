680acf4b6894acf0a418f49b41df2d83b0cc4182
Martin Ivanco <ivancom.fr@gmail.com>
2019-03-19 20:45:28 +0100

    Parsing and adding header done

--- dynamic_table.js
+++ dynamic_table.js
@@ -1,15 +1,27 @@
 function TableRow(rowId, trElement) {
     this.rowId = rowId;
+    this.attributes = trElement.attributes
     this.cells = [];
     var tdElements = trElement.getElementsByTagName("td");
     for (var i = 0; i < tdElements.length; i++) {
-        this.cells.push(tdElements[i].innerHTML);
+        this.cells.push(tdElements[i]);
     }
 }
 
 function DynamicTable(tableId) {
-    this.testFunc = function () {
-        console.log(this.rows);
+    this.redraw = function () {
+        console.log(this.filters);
+        console.log(this.orders);
+    }
+
+    this.setFilter = function (column) {
+        this.filters[column] = this.filterBar.childNodes[column].childNodes[0].value;
+        this.redraw();
+    }
+
+    this.setOrder = function (column, order) {
+        this.orders[column] = order;
+        this.redraw();
     }
 
     this.rows = []
@@ -20,6 +32,7 @@ function DynamicTable(tableId) {
                 var row = new TableRow(rowId, element);
                 this.rows.push(row);
                 rowId++;
+                // document.getElementById(tableId).removeChild(element);
             }
             if (element.tagName == "TBODY") {
                 var trElements = element.getElementsByTagName("tr");
@@ -28,6 +41,7 @@ function DynamicTable(tableId) {
                     this.rows.push(row);
                     rowId++;
                 }
+                // document.getElementById(tableId).removeChild(element);
             }
         }
     });
@@ -46,27 +60,44 @@ function DynamicTable(tableId) {
         this.orders[i] = 0;
     }
 
-    var filterBar = document.createElement("tr");
+    this.filterBar = document.createElement("tr");
     for (var i = 0; i < this.numCols; i++) {
         var filterInput = document.createElement("input");
         filterInput.setAttribute("type", "text");
-        filterInput.oninput = this.testFunc.bind(this);
+        filterInput.oninput = this.setFilter.bind(this, i);
         var cellElement = document.createElement("td");
         cellElement.appendChild(filterInput);
-        filterBar.appendChild(cellElement);
+        this.filterBar.appendChild(cellElement);
+    }
+
+    this.sortBar = document.createElement("tr");
+    for (var i = 0; i < this.numCols; i++) {
+        var cellElement = document.createElement("td");
+        for (var j = 0; j < 3; j++) {
+            var sortButton = document.createElement("button");
+            sortButton.setAttribute("class", "sort_button_" + j);
+            sortButton.onclick = this.setOrder.bind(this, i, j);
+            cellElement.appendChild(sortButton);
+        }
+        this.sortBar.appendChild(cellElement);
     }
     
     var tmp = document.getElementById(tableId).getElementsByTagName("thead");
     if (tmp.length < 1) {
-        document.getElementById(tableId).insertBefore(filterBar, document.getElementById(tableId).childNodes[0]);
+        document.getElementById(tableId).insertBefore(this.sortBar, document.getElementById(tableId).childNodes[0]);
+        document.getElementById(tableId).insertBefore(this.filterBar, document.getElementById(tableId).childNodes[0]);
     }
     else {
-        if (tmp[0].childNodes.length < 1)
-            tmp[0].appendChild(filterBar);
-        else
-            tmp[0].insertBefore(filterBar, tmp[0].childNodes[0]);
+        if (tmp[0].childNodes.length < 1) {
+            tmp[0].appendChild(this.filterBar);
+            tmp[0].appendChild(this.sortBar);
+        }
+        else {
+            tmp[0].insertBefore(this.sortBar, tmp[0].childNodes[0]);
+            tmp[0].insertBefore(this.filterBar, tmp[0].childNodes[0]);
+        }
     }
 }
 
 myTable = new DynamicTable("rubiks_cube");
-console.log(myTable);
+// console.log(myTable);
