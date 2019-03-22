04d5ee1aba7cc73149e66db668442a79b7b02314
Martin Ivanco <ivancom.fr@gmail.com>
2019-03-22 16:56:38 +0100

    Filtering working

--- dynamic_table.js
+++ dynamic_table.js
@@ -1,17 +1,98 @@
+const STRCOL = 1;
+const NUMCOL = 3;
+
 function TableRow(rowId, trElement) {
     this.rowId = rowId;
-    this.attributes = trElement.attributes
+    this.trElement = trElement;
     this.cells = [];
     var tdElements = trElement.getElementsByTagName("td");
     for (var i = 0; i < tdElements.length; i++) {
-        this.cells.push(tdElements[i]);
+        this.cells.push(tdElements[i].innerText);
     }
 }
 
 function DynamicTable(tableId) {
+    this.filterRows = function () {
+        var filteredRows = this.rows.slice();
+        for (var i = 0; i < this.filters.length; i++) {
+            if (this.filters[i] == "")
+                continue;
+
+            for (var j = 0; j < filteredRows.length; j++) {
+                if (! filteredRows[j].cells[i].includes(this.filters[i]))
+                    delete filteredRows[j];
+            }
+        }
+        return filteredRows;
+    }
+
+    this.sortRows = function (filteredRows) {
+        var originalOrder = true;
+        for (var i = 0; i < this.orders.length; i++) {
+            if (this.orders[i] != 0) {
+                originalOrder = false;
+                break;
+            }
+        }
+        if (originalOrder) {
+            var sortedRows = [];
+            filteredRows.forEach(row => {
+                sortedRows.push(row.trElement);
+            });
+            return sortedRows;
+        }
+
+        var tmpRows = filteredRows.slice();
+        for (var i = 0; i < this.orders.length; i++) {
+            tmpRows.sort((r1, r2) => {
+                if (this.orders[i] == 1) {
+                    if (r1.cells[i] < r2.cells[i])
+                        return -1;
+                    if (r1.cells[i] > r2.cells[i])
+                        return 1;
+                    return 0;
+                }
+                if (this.orders[i] == 2) {
+                    if (r1.cells[i] < r2.cells[i])
+                        return 1;
+                    if (r1.cells[i] > r2.cells[i])
+                        return -1;
+                    return 0;
+                }
+                if (this.orders[i] == 3) {
+                    if (Number(r1.cells[i]) < Number(r2.cells[i]))
+                        return -1;
+                    if (Number(r1.cells[i]) > Number(r2.cells[i]))
+                        return 1;
+                    return 0;
+                }
+                if (this.orders[i] == 6) {
+                    if (Number(r1.cells[i]) < Number(r2.cells[i]))
+                        return 1;
+                    if (Number(r1.cells[i]) > Number(r2.cells[i]))
+                        return -1;
+                    return 0;
+                }
+            });
+        }
+        var sortedRows = [];
+        tmpRows.forEach(row => {
+            sortedRows.push(row.trElement);
+        });
+        return sortedRows;
+    }
+
     this.redraw = function () {
-        console.log(this.filters);
-        console.log(this.orders);
+        while (this.tbody.firstChild) {
+            this.tbody.removeChild(this.tbody.firstChild);
+        }
+
+        var filteredRows = this.filterRows();
+        var orderedRows = this.sortRows(filteredRows);
+        
+        orderedRows.forEach(row => {
+            this.tbody.appendChild(row);
+        });
     }
 
     this.setFilter = function (column) {
@@ -24,30 +105,30 @@ function DynamicTable(tableId) {
         this.redraw();
     }
 
-    this.rows = []
-    var rowId = 0;
-    document.getElementById(tableId).childNodes.forEach(element => {
-        if (element.nodeType == Node.ELEMENT_NODE) {
-            if (element.tagName == "tr") {
-                var row = new TableRow(rowId, element);
-                this.rows.push(row);
-                rowId++;
-                // document.getElementById(tableId).removeChild(element);
-            }
-            if (element.tagName == "TBODY") {
-                var trElements = element.getElementsByTagName("tr");
-                for (var i = 0; i < trElements.length; i++) {
-                    var row = new TableRow(rowId, trElements[i]);
-                    this.rows.push(row);
-                    rowId++;
-                }
-                // document.getElementById(tableId).removeChild(element);
-            }
+    for (var i = 1; i < arguments.length; i++) {
+        if ((arguments[i] != STRCOL) && (arguments[i] != NUMCOL)) {
+            console.error("Dynamic table: Invalid arguments.");
+            return;
         }
-    });
-
-    if (this.rows.length < 1)
+    }
+    
+    var tmp = document.getElementById(tableId).getElementsByTagName("tbody");
+    if (tmp.length < 1) {
+        console.error("Dynamic table: No <tbody> element inside table.");
         return;
+    }
+    this.tbody = tmp[0];
+
+    this.rows = [];
+    var trElements = this.tbody.getElementsByTagName("tr");
+    for (var i = 0; i < trElements.length; i++) {
+        this.rows.push(new TableRow(i, trElements[i]));
+    }
+
+    if (this.rows.length < 1) {
+        console.error("Dynamic table: No rows inside <tbody> element.");
+        return;
+    }
     this.numCols = this.rows[0].cells.length;
 
     this.filters = [];
@@ -76,7 +157,10 @@ function DynamicTable(tableId) {
         for (var j = 0; j < 3; j++) {
             var sortButton = document.createElement("button");
             sortButton.setAttribute("class", "sort_button_" + j);
-            sortButton.onclick = this.setOrder.bind(this, i, j);
+            if (arguments.length > i + 1)
+                sortButton.onclick = this.setOrder.bind(this, i, j * arguments[i + 1]);
+            else
+                sortButton.onclick = this.setOrder.bind(this, i, j);
             cellElement.appendChild(sortButton);
         }
         this.sortBar.appendChild(cellElement);
@@ -99,5 +183,5 @@ function DynamicTable(tableId) {
     }
 }
 
-myTable = new DynamicTable("rubiks_cube");
-// console.log(myTable);
+myTable = new DynamicTable("rubiks_cube", NUMCOL, STRCOL, STRCOL, STRCOL);
+console.log(myTable);
